3099bb0fad4a6c7c19116dec1e4d8cc13847e51e
Renan <renansteles@gmail.com>
2021-04-02 20:44:57 -0300

    Starting learning AlpineJs

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Traning AlpineJs</title>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" />
+</head>
+<body>
+  <h1>Test</h1>
+</body>
+</html>

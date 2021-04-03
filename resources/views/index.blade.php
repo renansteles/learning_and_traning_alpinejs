62e48df62d28b4aac205b0e99ed1a790b208f112
Renan <renansteles@gmail.com>
2021-04-02 21:08:43 -0300

    simple binding example

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html dir="ltr" lang="en">
+<html dir="ltr" lang="pt-br">
 <head>
     <meta charset="UTF-8" />
     <title>Traning AlpineJs</title>
@@ -7,6 +7,19 @@
     <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" />
 </head>
 <body>
-  <h1>Test</h1>
+  <div x-data="{ message: 'Click Me' }" class="px-10 flex items-center justify-center min-h-screen">
+      <div class="flex-1 grid grid-cols-4 gap-10">
+        <div class="h-32 bg-gray-300">
+            <button @click="message = 'I have been clicked' " x-text="message"></button>
+        </div>
+        <div class="h-32 bg-gray-300"></div>
+        <div class="h-32 bg-gray-300"></div>
+        <div class="h-32 bg-gray-300"></div>
+        <div class="h-32 bg-gray-300"></div>
+        <div class="h-32 bg-gray-300"></div>
+        <div class="h-32 bg-gray-300"></div>
+        <div class="h-32 bg-gray-300"></div>
+      </div>
+  </div>
 </body>
 </html>
